2ffd04a3120599aeb549dffcb088a3eee8eaeb03
Tks23-hub <deathnotekiller23@gmail.com>
2023-09-20 22:07:33 +0300

    added a ejs page with 2 buttons that allow me to clock in and clock out of work and maid the necessary changes for the code to work

--- views/FE_R.js
+++ views/FE_R.js
@@ -7,3 +7,9 @@ router.get("/",(req, res) => {
     res.render("mainPage", {pageTitle:"בוקר טוב"});
 
 });
+
+router.get("/enter-leave",(req, res) => {
+
+    res.render("enter-leave", {pageTitle:"בוקר טוב"});
+
+});
