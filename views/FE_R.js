d54000f33b9c831c66818566a0d1584896f52056
Tks23-hub <128156006+Tks23-hub@users.noreply.github.com>
2023-09-24 09:52:35 +0300

    FE_R.js

--- views/FE_R.js
+++ views/FE_R.js
@@ -3,22 +3,12 @@ const router = express.Router()
 module.exports = router;
 
 router.get("/",(req, res) => {
-
     res.render("mainPage", {pageTitle:"בוקר טוב"});
-
 });
-
 router.get("/enter-leave",(req, res) => {
-
     res.render("enter-leave", {pageTitle:"בוקר טוב"});
-
 });
-
 
 router.get("/shifts",(req, res) => {
-
-    // Assuming 'workers' is an array of objects with 'id' and 'name' properties
     res.render('shifts', { workers: "workers" });
-
-
 });
