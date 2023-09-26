a327d69ae7f4ff73439f30fc4a9c67fde599472a
Tks23-hub <deathnotekiller23@gmail.com>
2023-09-26 07:34:12 +0300

    this is the main index that contains the port   and i just modified the commit coment

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const port = 7575;
 const app = express();
 app.use(express.json());
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
